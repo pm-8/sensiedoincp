d094575da40ca11b11499bd4975496cd159693c1
Prakhar <mishraprakhar150@gmail.com>
2024-10-26 01:19:19 +0530

    Fast Power

---
+++ Recursion/FastPower.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fastPower(int a, int n){
+    if(n == 0) return 1;
+    int subProb = fastPower(a,n/2);
+    int subProbSq = subProb*subProb;
+    if(n&1){
+        return a*subProbSq;
+    }
+    return subProbSq;
+}
+int main(){
+    int a,n;
+    cin >> a >> n;
+    cout << fastPower(a,n) << endl;
+    return 0;
+}
