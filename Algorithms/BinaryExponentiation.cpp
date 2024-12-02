d992f726ebabd40bc43f142df894d144dd5fb531
Prakhar <mishraprakhar150@gmail.com>
2024-12-02 23:36:37 +0530

    Committed

---
+++ Algorithms/BinaryExponentiation.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+long long recursive_power(long long A, long long B){
+    if(B==0){
+        return 1;
+    }
+    long long res = recursive_power(A,B/2);
+    if(B%2)
+    return res * res * A;
+    else 
+    return res * res;
+}
+long long iterative_power(long long a, long long b){
+    long long res = 1;
+    while(b){
+    if(b&1)
+    res *= a;
+    a*=a;
+    b>>=1;
+    }
+    return res;
+}
+int main()
+{
+    int b =8;
+    while(b){
+        cout << b << endl;
+        b >>= 1;
+    }
+    return 0;
+}
