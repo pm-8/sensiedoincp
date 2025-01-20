30eb2b653329b85c14d7b668f7b115efb4136a1e
Prakhar <mishraprakhar150@gmail.com>
2025-01-21 02:49:26 +0530

    changes made

--- Dynamic_Programming.cpp/Subset_sum_top_down_approach.cpp
+++ Dynamic_Programming.cpp/Subset_sum_top_down_approach.cpp
@@ -1,20 +1,24 @@
 #include<bits/stdc++.h>
+#define int long long int
 #define N 10000
 using namespace std;
-int main(){
-    int n,sum;
-    cin >> n >> sum;
-    int arr[n];
-    for(int i = 0; i < n; i++) cin >> arr[i];
-    vector<vector<int>> memo(n+1,vector<int>(sum+1,-1));
-    function<bool(int,int)> isSubset = [&](int i, int s){
-        //if the sum is zero, there is a subset
-        if(s == 0) return 1;
-        //if no elements left, then for sure it's fucked up
-        if(i <= 0) return 0;
-        if(memo[i][s] != -1) return memo[i][s];
-        if(arr[i] > sum) memo[i][s] = isSubset(i-1,s);
-        else memo[i][s] = isSubset(i-1,s - arr[i-1]);
-    };
-    cout << (bool)isSubset(n,sum) << endl;
+int32_t main(){
+    int n;
+    cin >> n;
+    int count = 0;
+    unordered_map<int,int> mp;
+    for(int i = 1; i <= n; i++){
+        for(int j = i+1; j <= n; j++){
+            int c = (int)(i ^ j);
+            if(c > n) continue;
+            if((i+j>c) && (i+c>j) && (c+j>i) && (mp[i] == 0 || mp[j] == 0 || mp[c] == 0)){
+                mp[i]=1;
+                mp[j]=1;
+                mp[c]=1;
+                count++;
+            }
+        }
+    }
+    cout << count << endl;
+    return 0;
 }
--- Dynamic_Programming.cpp/knapsack+-+1.cpp
+++ Dynamic_Programming.cpp/knapsack+-+1.cpp
@@ -1,49 +1,117 @@
-#include<bits/stdc++.h>
-
-#define int long long int
-#define F first
-#define S second
-#define pb push_back
-
+#include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-const int N = 100;
+// Union-Find (Disjoint Set Union) structure
+struct UnionFind {
+    vector<int> parent, rank;
 
-int v[N], w[N], W, n, memo[N][100001];
+    UnionFind(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
 
-// it returns the maximum value total value
-int knapsack(int index, int weight) {
-	if (index >= n) return 0;
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); // Path compression
+        }
+        return parent[x];
+    }
 
-	// memoissatioon part
-	int &ans = memo[index][weight];
-	if (ans != -1) return ans;
+    bool unionSets(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] < rank[rootY]) {
+                swap(rootX, rootY);
+            }
+            parent[rootY] = rootX;
+            if (rank[rootX] == rank[rootY]) {
+                rank[rootX]++;
+            }
+            return true; // Union performed
+        }
+        return false; // Already in the same set
+    }
 
-	// do not take the item
-	ans = knapsack(index + 1, weight);
-	// take the item at index
-	if (w[index] + weight <= W) {
-		ans = max(ans, v[index] + knapsack(index + 1, w[index] + weight));
-	}
-	return ans;
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); // Path compression
+        }
+        return parent[x];
+    }
+};
+
+void solve() {
+    int n, m1, m2;
+    cin >> n >> m1 >> m2;
+
+    UnionFind ufF(n), ufG(n);
+
+    for (int i = 0; i < m1; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--, v--;
+        ufF.unionSets(u, v);
+    }
+
+    for (int i = 0; i < m2; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--, v--;
+        ufG.unionSets(u, v);
+    }
+
+    vector<pair<int, int>> operations;
+
+    map<int, vector<int>> componentsG;
+    for (int i = 0; i < n; i++) {
+        componentsG[ufG.find(i)].push_back(i);
+    }
+
+    for (auto &[gRoot, gNodes] : componentsG) {
+        set<int> fRoots;
+        for (int node : gNodes) {
+            fRoots.insert(ufF.find(node));
+        }
+
+        vector<int> fRootsList(fRoots.begin(), fRoots.end());
+        if (fRootsList.size() > 1) {
+            int primaryRoot = fRootsList[0];
+            for (size_t i = 1; i < fRootsList.size(); i++) {
+                int currentRoot = fRootsList[i];
+                int u = -1, v = -1;
+                for (int node : gNodes) {
+                    if (ufF.find(node) == primaryRoot) u = node;
+                    if (ufF.find(node) == currentRoot) v = node;
+                }
+                if (u != -1 && v != -1) {
+                    operations.push_back({u, v});
+                    ufF.unionSets(u, v);
+                }
+            }
+        }
+    }
+
+    cout << operations.size() << "\n";
+    for (auto &[u, v] : operations) {
+        cout << u + 1 << " " << v + 1 << "\n"; // Convert back to 1-based index
+    }
 }
 
 int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
 
-	cin >> n >> W;
-
-	for (int i = 0; i < n; i++) {
-		cin >> w[i] >> v[i];
-	}
-
-	memset(memo, -1, sizeof(memo));
-
-	cout << knapsack(0, 0);
-
-	return 0;
+    return 0;
 }
